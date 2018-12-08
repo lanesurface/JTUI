9cdeda7767aa9fe1bf2621e12f9989fbce012085
Lane W. Surface <surfacelane@gmail.com>
2018-12-08 17:08:03 -0600

    improved test script somewhat

--- tools/test-api.py
+++ tools/test-api.py
@@ -6,30 +6,27 @@ is returned.
 """
 
 import os, sys
-import re, argparse
+import argparse
 import subprocess
 
 root_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 
-# All tests must reside in the path '../src/test'. Files in this package
-# directory will be built automatically and verified against the API.
+# The package in the source folder that holds the tests to run against the API.
+# Classes in this package will be built automatically.
+package = 'test'
 test_path = os.sep.join([root_dir,
                          'src',
-                         'test'])
+                         package])
 tests = [name for _, _, name in os.walk(test_path)][0]
 
 # The directories that will be used when building the classpath.
 include_dirs = [os.path.join(root_dir, include) for include in
                 ['res',
                  'src']]
+include_dirs.append('.')
+classpath = os.pathsep.join(include_dirs)
 
-package = 'test/'
-package_name = re.sub('[/\\\\]', '.', package)
-
-classpath = ['.', root_dir]
-classpath.extend(include_dirs)
-
-print('Using classpath %s' % classpath)
+print('Using classpath "%s"' % classpath)
 
 # Verify all tests pass before creating a project. If the library is
 # configured incorrectly, this will catch it before it causes confusion when
@@ -39,21 +36,20 @@ for test in tests:
     if not os.path.exists('../build'):
       os.mkdir('../build')
 
-    paths = ';'.join(classpath)
-
     os.chdir('../src')
     subprocess.call(['javac',
                      '-d',
                      '../build',
                      '-cp',
-                     paths,
-                     package + test])
+                     classpath,
+                     os.sep.join([package, test])])
 
     os.chdir('../build')
     exit_code = subprocess.call(['java',
                                  '-cp',
-                                 paths,
-                                 package_name + test.split('.')[0]])
+                                 classpath,
+                                 os.extsep.join([package,
+                                                 test.split('.')[0]])])
     if exit_code != 0:
       sys.stderr.write('Could not run the test {}.'.format(test))
       sys.exit(exit_code)

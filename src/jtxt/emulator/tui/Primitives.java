3cf8b3d2810dd19c27c85bad9e1d7ccb4c396d4d
Lane W. Surface <surfacelane@gmail.com>
2019-09-07 12:25:37 -0500

    Add circle primitive and fix DDA

---
+++ src/jtxt/emulator/tui/Primitives.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2019 Lane W. Surface
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package jtxt.emulator.tui;
+
+import static java.lang.Math.abs;
+
+/**
+ * Implementations for various utility methods which draw shapes (using
+ * characters for strokes) to the given {@code Frame}.
+ */
+public final class Primitives {
+  public static void rect(
+    Frame frame,
+    int x,
+    int y,
+    int w,
+    int h)
+  {
+
+  }
+
+  public static void circle(
+    Frame frame,
+    int xc,
+    int yc,
+    int r)
+  {
+    int x,
+      y,
+      d;
+
+    x = 0;
+    y = r;
+    d = 3 - 2 * r;
+    while (y >= x) {
+      plot(
+        frame,
+        xc,
+        yc,
+        x,
+        y);
+      x++;
+
+      if (d > 0) {
+        y--;
+        d = d + 4 * (x-y) + 10;
+      }
+      else {
+        d = d + 4*x + 6;
+      }
+    }
+  }
+
+  private static void plot(
+    Frame frame,
+    int xc,
+    int yc,
+    int x,
+    int y)
+  {
+    frame.set(null, xc+x, yc+y);
+    frame.set(null, xc-x, yc+y);
+    frame.set(null, xc+x, yc-y);
+    frame.set(null, xc-x, yc-y);
+    frame.set(null, xc+y, yc+x);
+    frame.set(null, xc-y, yc+x);
+    frame.set(null, xc+y, yc-x);
+    frame.set(null, xc-y, yc-x);
+  }
+
+  public static void line(
+    Frame frame,
+    int sx,
+    int sy,
+    int ex,
+    int ey)
+  {
+    int steps,
+      dx,
+      dy;
+    double xi,
+      yi,
+      x,
+      y;
+    char ch;
+
+    dx = ex - sx;
+    dy = ey - sy;
+    steps = Math.max(
+      abs(dx),
+      abs(dy));
+    xi = dx / (steps+0.d);
+    yi = dy / (steps+0.d);
+    x = y = 0;
+    for (int i = 0; i < steps; i++) {
+      x += xi;
+      y += yi;
+      frame.set(
+        null,
+        (int)x,
+        (int)y);
+    }
+  }
+}

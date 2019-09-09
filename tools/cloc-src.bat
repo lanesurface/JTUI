36b30d1fd36906f480872e28e76d5a2e70c91bd8
Lane W. Surface <surfacelane@gmail.com>
2019-09-08 19:48:46 -0500

    Remove legacy tooling and stale configs

--- tools/cloc-src.bat
+++
@@ -1,3 +0,0 @@
-REM NOTE: Cloc must be installed on your system and added to the `PATH`
-REM       environment variable before this script can be run.
-cloc .. --exclude-dir=docs --out=stats.txt --csv
